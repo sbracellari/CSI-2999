76ebc352da0e77dc4748788a525993b8b7d52cf2
kmasiak <38893432+kmasiak@users.noreply.github.com>
2019-02-10 18:21:46 -0500

    Added 'Snowday Calculator' title and a 'Calculate' Button

--- snowday-calc/src/App.js
+++ snowday-calc/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+	  <div className="App">
+		<header className="App-header">
+			<h1>
+				Snowday Calculator
+			</h1>
+			<Button id="btn_calc" variant="contained" size="large" color="primary">
+				Calculate
+			</Button>
+		</header>
+	  </div>
     );
   }
 }
