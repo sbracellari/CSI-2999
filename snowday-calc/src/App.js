822f3b1687cb1e53ab79c7dc738654095d5394cd
Samantha Bracellari <sbracellari@oakland.edu>
2019-02-12 11:27:33 -0500

    basic frontend design #3

--- snowday-calc/src/App.js
+++ snowday-calc/src/App.js
@@ -1,24 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import PropTypes from 'prop-types';
+//import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 
-class App extends Component {
+function Transition(props) {
+  return <Slide direction="down" {...props} />;
+}
+
+class App extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Snow Day Calculator</p>
+          <Button variant="outlined" className="btn-calc" onClick={this.handleClickOpen}>
+            Calculate</Button>
+          <Dialog
+            open={this.state.open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={this.handleClose}
+            aria-labelledby="calc-dialog-slide-title"
+            aria-describedby="calc-dialog-slide-description">
+
+            <DialogTitle id="calc-dialog-slide-title">
+              {"Your chances of a snow day are..."}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="calc-dialog-slide-description">
+                Based on your input, the chance of a snow day on (selected-date) 
+                in (selected-area) is (result-percent).
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Close
+              </Button>
+            </DialogActions>
+          </Dialog>
         </header>
       </div>
     );
